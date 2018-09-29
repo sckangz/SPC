22e406f6335ae9ca7bc1ad7dccd58d9f0cb06cf3
Zhao Kang 康昭 <sckangzjobs@gmail.com>
2018-09-29 11:21:46 +0800

    Update runmultiple3.m

--- runmultiple3.m
+++ runmultiple3.m
@@ -2,9 +2,10 @@
 warning off
 % load ('jaffe_213n_676d_10c_uni.mat');
 % load ('jaffe_213n_676d_10c_uni_kernel_gaussian_0.01_post_Sample-Scale.mat');
-para1=[1 10 ];
-para2=[ .0001 .001  ];
-para3=[ .0001  .001];
+% you should tune these parameters to obtain the best performance.
+para1=[ 10 ];
+para2=[ .0001   ];
+para3=[  .001];
 
 
 for i=1:length(para1)
